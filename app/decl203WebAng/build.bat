16bf8abb2fdf18f61cff96a9e513d80976b27c79
ignatandrei <ignatandrei@yahoo.com>
2018-04-10 09:30:09 +0300

    added cordova

--- app/decl203WebAng/build.bat
+++ app/decl203WebAng/build.bat
@@ -2,6 +2,6 @@ call ng build --prod --build-optimizer
 node node_modules\angular-cli-ghpages\bin\angular-cli-ghpages
 rem robocopy dist ..\decl203Electron\app\dist  /MIR /XD
 
-rem robocopy dist ..\declCordova\www\dist  /MIR /XD
+robocopy dist ..\decl203Cordova\www\dist  /MIR /XD
 
 rem echo 'modify href FOR CORDOVA TO  .!'
