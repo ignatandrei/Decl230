760db38506e759098f8cea947f7393add7ae6853
ignatandrei <ignatandrei@yahoo.com>
2018-04-11 00:29:37 +0300

    building with electron

--- app/decl203WebAng/build.bat
+++ app/decl203WebAng/build.bat
@@ -1,11 +1,17 @@
  call ng build --prod --build-optimizer
 
+
+ robocopy dist ..\decl203Cordova\www\dist  /MIR /XD
+
+ echo '!!!modify href FOR CORDOVA TO  .!'
+
+pause
  robocopy dist ..\decl203Electron\app\dist  /MIR /XD
 cd ..\decl203Electron
 
  call npm run release
 
- robocopy dist ..\decl203WebAng\dist\electron  /MIR /XD
+ copy dist\*.* ..\decl203WebAng\dist\  /MIR /XD
 
 rmdir ..\decl203WebAng\dist\electron\win-unpacked\ /s /q
 
@@ -14,6 +20,3 @@ cd ..\decl203WebAng
 
 node node_modules\angular-cli-ghpages\bin\angular-cli-ghpages
 
- robocopy dist ..\decl203Cordova\www\dist  /MIR /XD
-
- echo 'modify href FOR CORDOVA TO  .!'
