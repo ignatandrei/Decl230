ec05566d14c2853890e80292cef05669185b649c
ignatandrei <ignatandrei@yahoo.com>
2018-04-10 23:55:34 +0300

    electron

---
+++ app/decl203Electron/src/hello_world/hello_world.js
@@ -0,0 +1,7 @@
+export const greet = () => {
+  return "Declaratia 230";
+};
+
+export const bye = () => {
+  return "See ya!";
+};
--- app/decl203WebAng/build.bat
+++ app/decl203WebAng/build.bat
@@ -1,7 +1,19 @@
-call ng build --prod --build-optimizer
+ call ng build --prod --build-optimizer
+
+ robocopy dist ..\decl203Electron\app\dist  /MIR /XD
+cd ..\decl203Electron
+
+ call npm run release
+
+ robocopy dist ..\decl203WebAng\dist\electron  /MIR /XD
+
+rmdir ..\decl203WebAng\dist\electron\win-unpacked\ /s /q
+
+cd ..\decl203WebAng
+
+
 node node_modules\angular-cli-ghpages\bin\angular-cli-ghpages
-rem robocopy dist ..\decl203Electron\app\dist  /MIR /XD
 
-robocopy dist ..\decl203Cordova\www\dist  /MIR /XD
+ robocopy dist ..\decl203Cordova\www\dist  /MIR /XD
 
-rem echo 'modify href FOR CORDOVA TO  .!'
+ echo 'modify href FOR CORDOVA TO  .!'
