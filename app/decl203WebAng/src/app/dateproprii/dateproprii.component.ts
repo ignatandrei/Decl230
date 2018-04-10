24ed702daeb07bb529322511ef30e5d562c44104
ignatandrei <ignatandrei@yahoo.com>
2018-04-10 09:18:30 +0300

    adaugat apk si persoana

--- app/decl203WebAng/src/app/dateproprii/dateproprii.component.ts
+++ app/decl203WebAng/src/app/dateproprii/dateproprii.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, OnChanges, SimpleChanges, Output ,EventEmitter} from '@angular/core';
+import { Component, OnInit, Inject, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 import { Persoana } from './persoana';
 
 
@@ -9,32 +9,50 @@ import { Persoana } from './persoana';
 })
 
 export class DatepropriiComponent implements OnInit, OnChanges {
-  constructor(@Inject('LOCALSTORAGE') private localStorage: any) { 
-    this.pers=new Persoana();
+  constructor(@Inject('LOCALSTORAGE') private localStorage: any) {
+    this.pers = new Persoana();
   }
 
-  @Output() onPersoanaSelected=new EventEmitter<Persoana>();
-  
-  pers:Persoana;
-  public get isValid():boolean
-    {
-      return this.pers.isValid;
-    }
+  @Output() onPersoanaSelected = new EventEmitter<Persoana>();
+
+  pers: Persoana;
+  public get isValid(): boolean {
+    return this.pers.isValid;
+  }
+  public get DeCompletat(): string {
+    return this.pers.DeCompletat;
+  }
   ngOnChanges(changes: SimpleChanges): void {
-    
-    if(this.localStorage){
-      localStorage.setItem("nume", this.pers.nume);  
-      localStorage.setItem("prenume", this.pers.prenume);  
+
+    if (this.localStorage) {
+      localStorage.setItem("nume", this.pers.nume);
+      localStorage.setItem("prenume", this.pers.prenume);
     }
   }
-  saveChanges(proName:string){
-    localStorage.setItem(proName, this.pers[proName]);    
-   this.onPersoanaSelected.emit(this.pers);
+  saveChanges(proName: string) {
+    localStorage.setItem(proName, this.pers[proName]);
+    this.onPersoanaSelected.emit(this.pers);
   }
 
   ngOnInit() {
-    this.pers.nume=localStorage.getItem("nume");
-    this.pers.prenume=localStorage.getItem("prenume");
+    this.pers.nume = localStorage.getItem("nume");
+    this.pers.prenume = localStorage.getItem("prenume");
+    this.pers.initialatatalui = localStorage.getItem("initialatatalui");
+    this.pers.strada = localStorage.getItem("strada");
+    this.pers.numar = localStorage.getItem("numar");
+    this.pers.bloc = localStorage.getItem("bloc");
+    this.pers.scara = localStorage.getItem("scara");
+    this.pers.etaj = localStorage.getItem("etaj");
+    this.pers.ap = localStorage.getItem("ap");
+    this.pers.judet = localStorage.getItem("judet");
+    this.pers.localitate = localStorage.getItem("localitate");
+    this.pers.codpostal = localStorage.getItem("codpostal");
+    this.pers.cnp = localStorage.getItem("cnp");
+    this.pers.email = localStorage.getItem("email");
+    this.pers.telefon = localStorage.getItem("telefon");
+    this.pers.fax = localStorage.getItem("fax");
+
+
     this.onPersoanaSelected.emit(this.pers);
   }
 
--- app/decl203WebAng/src/app/dateproprii/persoana.ts
+++ app/decl203WebAng/src/app/dateproprii/persoana.ts
@@ -1,38 +1,51 @@
-export class Persoana{
-    constructor(){
-        this.nume='';
-        this.prenume='';
-        
-    }
-    public nume:string;
-    public prenume:string;
-    public initialatatalui:string;
-    public strada:string;
-    public numar:string;
-    public bloc:string;
-    public scara:string;
-    public etaj:string;
-    public ap:string;
-    public judet: string;
-    public localitate:string;
-    public codpostal: string;
-    public cnp:string;
-    public email: string;
-    public telefon:string;
-    public fax: string;
-    exists(x:string):boolean
-    {
-        return (x||'').length>0;
-    }
-    public get isValid():boolean    
-    {
+export class Persoana {
 
-        return this.exists(this.nume) 
-        && this.exists(this.prenume)
-        && this.exists(this.judet)
-        && this.exists(this.localitate)
-        && this.exists(this.cnp)
-        && this.exists(this.email + this.telefon + this.fax)
-        ;
+    constructor() {
+        this.nume = '';
+        this.prenume = '';
+
+    }
+    public nume: string;
+    public prenume: string;
+    public initialatatalui: string;
+    public strada: string;
+    public numar: string;
+    public bloc: string;
+    public scara: string;
+    public etaj: string;
+    public ap: string;
+    public judet: string;
+    public localitate: string;
+    public codpostal: string;
+    public cnp: string;
+    public email: string;
+    public telefon: string;
+    public fax: string;
+    exists(x: string): boolean {
+        return (x || '').length > 0;
+    }
+    public get isValid(): boolean {
+
+        return !this.exists(this.DeCompletat);
+
+    }
+
+    public get DeCompletat(): string {
+
+        if (!this.exists(this.nume))
+            return "nume";
+
+        if (!this.exists(this.prenume))
+            return "prenume";
+        if (!this.exists(this.judet))
+            return "judet";
+        if (!this.exists(this.localitate))
+            return "localitate";
+        if (!this.exists(this.cnp))
+            return "cnp";
+        if (!this.exists(this.email + this.telefon + this.fax))
+            return "email, fax sau telefon";
+
+        return "";
     }
 }
