58d88b5e74aa44aca4fbf839fce4d37572071ee8
ignatandrei <ignatandrei@yahoo.com>
2018-05-15 05:25:18 +0300

    update logo many more

--- app/decl203Cordova/build.bat
+++ app/decl203Cordova/build.bat
@@ -1,1 +1,6 @@
-call cordova build android
+call cordova build android --release
+start /b /wait /d . CMD /c %localappdata%\Android\Sdk\build-tools\27.0.3\apksigner.bat sign -ks D:\github\all.jks platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
+
+start /b /wait /d . CMD /c %localappdata%\Android\Sdk\build-tools\27.0.3\apksigner.bat verify --verbose platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk
+
+explorer platforms\android\app\build\outputs\apk\release\
